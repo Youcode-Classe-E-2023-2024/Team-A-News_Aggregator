044b76c9804b0176c026c269d1a70a5bf68ef10b
ELMEHDI_ELFARI <144785903+ElmehdiElfari@users.noreply.github.com>
2024-02-21 10:32:07 +0100

    first work done

---
+++ Aggregator_App/resources/views/pages/register.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+
+    <title>Document</title>
+</head>
+<body class="bg-gray-800">
+<div class=" font-[sans-serif] text-[#333] mt-4 p-4 relative">
+    <div class="max-w-md w-full mx-auto relative z-50">
+        <div class="text-center mb-8">
+            <a href="javascript:void(0)"><img
+                    src="https://readymadeui.com/readymadeui.svg" alt="logo" class='w-48 inline-block' />
+            </a>
+        </div>
+        <x-sign_up />
+        </form>
+    </div>
+</div>
+</body>
+</html>
+
