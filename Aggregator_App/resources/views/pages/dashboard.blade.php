d01f8adb428ebee946ef07e0360eaed8c23e25c1
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-02-21 15:08:16 +0100

    popup for changing the role

--- Aggregator_App/resources/views/pages/dashboard.blade.php
+++ Aggregator_App/resources/views/pages/dashboard.blade.php
@@ -67,16 +67,21 @@
             </div>
 
             <!-- Task Summaries -->
-            <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-1 p-4 gap-4 text-black dark:text-white">
-                <div class="md:col-span-2 xl:col-span-3">
-                    <h3 class="text-lg font-semibold">Flux RSS recent Links</h3>
-                    <form class="" action="" method="post">
-                            <input type="text" name="name" id="name" class="mt-2 p-2 border border-gray-300 focus:outline-none focus:ring-0 focus:border-gray-300 rounded text-sm text-gray-900" placeholder="Enter your name">
-                            <button class="bg-indigo-600 hover:bg-indigo-700 rounded-lg px-8 py-2 text-gray-100 text-sm hover:shadow-xl transition duration-150 uppercase">Sign Up</button>
+            <div class="grid grid-cols-1 md:grid-cols-1 xl:grid-cols-1 gap-4 text-black dark:text-white">
+                <h3 class="ml-5 text-lg font-semibold">Flux RSS recent Links</h3>
+                <div class="ml-5 flex items-center">
+                    <input type="text" name="name" id="name"
+                           class="w-80 mt-2 p-2 border border-gray-300 focus:outline-none focus:ring-0 focus:border-gray-300 rounded-l-md text-sm text-gray-900"
+                           placeholder="Enter The URL">
+                    <form action=""
+                          method="post"
+                          class="bg-indigo-600 hover:bg-indigo-700 rounded-r-md px-4 py-2 text-gray-100 text-sm hover:shadow-xl transition duration-150 uppercase">
+                        @csrf
+                            Submit
                     </form>
                 </div>
 
-                <div class="md:col-span-2 xl:col-span-1">
+                <div class="ml-5 mr-5 md:col-span-2 xl:col-span-1">
                     <div class="rounded bg-gray-200 dark:bg-gray-800 p-3">
                         <div class="text-sm text-black dark:text-gray-50 mt-2">
                             <div
@@ -116,6 +121,7 @@
                                 <th class="px-4 py-3">Image</th>
                                 <th class="px-4 py-3">Name</th>
                                 <th class="px-4 py-3">Email</th>
+                                <th class="px-4 py-3">Role</th>
                                 <th class="px-4 py-3">Created date</th>
                                 <th class="px-4 py-3 text-center">Operation</th>
                             </tr>
@@ -135,16 +141,20 @@
                                 </td>
                                 <td class="px-4 py-3 text-sm">hafsa</td>
                                 <td class="px-4 py-3 text-sm">hafsa@gmail.com</td>
+                                <td class="px-4 py-3 text-sm">Admin</td>
                                 <td class="px-4 py-3 text-sm">15-01-2021</td>
                                 <td class="px-4 py-3 text-xs text-center">
                                     <button type="submit"
                                             class=" bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded">
                                         Delete User
                                     </button>
-                                    <button type="submit"
-                                            class=" bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
+                                    <button data-modal-target="crud-modal"
+                                            data-modal-toggle="crud-modal"
+                                            class=" bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+                                            type="button">
                                         Update User
                                     </button>
+
                                 </td>
                             </tr>
                             </tbody>
@@ -163,7 +173,59 @@
 </div>
 </div>
 
+
+<div id="crud-modal" tabindex="-1" aria-hidden="true"
+     class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-md max-h-full">
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                    Create New Product
+                </h3>
+                <button type="button"
+                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                        data-modal-toggle="crud-modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                         viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <form class="p-4 md:p-5">
+                <div class="grid gap-4 mb-4 grid-cols-1">
+                    <div class="sm:col-span-1">
+                        <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                        <select id="category"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            <option selected="">Select category</option>
+                            <option value="TV">Admin</option>
+                            <option value="PC">User</option>
+                        </select>
+                    </div>
+                </div>
+                <button type="submit"
+                        class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                              d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                              clip-rule="evenodd"></path>
+                    </svg>
+                    Change the Role
+                </button>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet"/>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.0/dist/alpine.min.js" defer></script>
+
 <script>
     const setup = () => {
         const getTheme = () => {
