d44ec5d0921a6320dff7fc71e5a032b9da00d7d4
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-02-21 11:32:26 +0100

    component de link

--- Aggregator_App/resources/views/components/navbar_home.blade.php
+++ Aggregator_App/resources/views/components/navbar_home.blade.php
@@ -1,24 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <!-- Required meta tags -->
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <!-- Title  -->
-  <title>Home Pages</title>
-  <meta name="description" content="News">
-
-  <!-- Development css -->
-  <link rel="stylesheet" href="src/css/style.css">
-  <!-- Production css -->
-  <!-- <link rel="stylesheet" href="dist/css/style.css"> -->
-  <!-- google font -->
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;600;700&amp;display=swap" rel="stylesheet">
-  <!-- Favicon  -->
-
-</head>
 
 <header class="fixed top-0 left-0 right-0 z-50">
     <nav class="bg-black">
--- Aggregator_App/resources/views/pages/home.blade.php
+++ Aggregator_App/resources/views/pages/home.blade.php
@@ -1,27 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <!-- Required meta tags -->
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@include('components.link_home')
 
-  <!-- Title  -->
-  <title>Home Pages</title>
-  <meta name="description" content="News">
+<x-link_home />
 
-  <!-- Development css -->
-  <link rel="stylesheet" href="src/css/style.css">
-  <!-- Production css -->
-  <!-- <link rel="stylesheet" href="dist/css/style.css"> -->
-  <!-- google font -->
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;600;700&amp;display=swap" rel="stylesheet">
-  <!-- Favicon  -->
-
-</head>
 <body class="text-gray-700 pt-9 sm:pt-10">
    <!-- =========={ NAVBAR }==========  -->
-  @include('components.navbar_home')
+
   <x-navbar_home />
   
   <!-- =========={ MAIN }==========  -->
