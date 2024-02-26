07409cd204b15be7b574ab197b3c6aab60274a12
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-02-26 09:45:30 +0100

    data base creation

--- aggregator_app/config/database.php
+++ aggregator_app/config/database.php
@@ -63,17 +63,15 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
+        'postgres' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'your_database_name'),
+            'username' => env('DB_USERNAME', 'your_database_username'),
+            'password' => env('DB_PASSWORD', 'your_database_password'),
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'prefer',
         ],
