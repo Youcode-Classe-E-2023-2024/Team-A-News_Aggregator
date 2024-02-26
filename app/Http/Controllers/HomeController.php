edfd0a7aaf91e8afe4960dd68033ef91b04742a4
Anass Benfillous <anasse.benfillous@gmail.com>
2024-02-26 17:02:25 +0100

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,26 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\News;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use SimplePie\Item;
 
 class HomeController extends Controller
 {
     function index()
     {
-        $items = News::query()
-            ->select('news.*', 'categories.*')
-            ->leftJoin('categories', 'news.category', '=', 'categories.id')
-            ->orderBy('news.created_at', 'desc')
-            ->get()
-            ->toArray();
-
-        if (!empty($items)) {
+        $items = $this->getCachedData();
+        if (count($items) !== 0) {
             $mainHero = $items[0];
             $fourHeroes = array_slice($items, 1, 4);
             return view('pages.home', compact('fourHeroes', 'mainHero'));
         }
         return view('pages.home');
+    }
+
+    function getCachedData()
+    {
+        $userId = Auth::id();
+        $user = User::with('userinterest')->find($userId);
+
+        $preferredCategoryIds = isset($user) ? $user->userinterest->pluck('category_id') : [];
+
+        if (count($preferredCategoryIds) === 0) {
+            $preferredCategoryIds = Category::all()->pluck('id')->toArray();
+            // here to make the checker true
+        }
+        $cacheKey = Auth::check() ? "user_{$user->id}_categories_" . implode(',', $preferredCategoryIds->flatten()->toArray()) : '';
+        $filteredNews = Cache::get($cacheKey);
+        if (!$filteredNews) {
+            $filteredNews = News::whereHas('category', function ($query) use ($preferredCategoryIds) {
+                $query->whereIn('id', $preferredCategoryIds);
+            })
+                ->with('category')
+                ->get()
+                ->toArray();
+
+            Cache::put($cacheKey, $filteredNews, 600); // Cache for 600 minutes
+        }
+        return $filteredNews;
     }
 }
