903dbed5dc40ddc82e51ddea4f97afae8ad24b9c
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-02-26 17:34:32 +0100

    show function

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
+use App\Models\News;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
+    public function show()
+    {
+        $comments = Comment::all();
+        return view('pages.detailed',compact('comments'));
+    }
+
     public function store(Request $request)
     {
         $request->validate([
