4007000a0d839c40640ebbe9323232a91a493c05
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-02-27 11:10:43 +0100

    table

--- resources/views/pages/emails.blade.php
+++ resources/views/pages/emails.blade.php
@@ -31,7 +31,7 @@
                                 <table class="w-full">
                                     <thead>
                                         <tr
-                                            class="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
+                                            class="text-md font-semibold tracking-wide text-center  text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
                                             <th class="px-4 py-3">Name</th>
                                             <th class="px-4 py-3">Email</th>
                                             <th class="px-4 py-3">Send</th>
