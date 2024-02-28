5d000fe8f45b4e4a95778ed90470070e0bcb8441
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-02-28 17:01:43 +0100

    add a service provider

---
+++ app/Providers/ViewServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Category;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+
+class ViewServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $categories = Category::all();
+        View::composer('*', function ($view) use ($categories) {
+            $view->with('categories', $categories);
+        });
+    }
+}
