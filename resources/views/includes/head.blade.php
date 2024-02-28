6cb9e7d91e48382220e91c89a002bf8d84109141
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-02-28 11:06:37 +0100

    adding jquery scrpit

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -5,3 +5,4 @@
 <title></title>
 <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
